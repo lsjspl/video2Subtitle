deaf7403c0bb72341bda91d06c9d9fe9d162c60e
lsjspl <lsjspl@qq.com>
2023-12-09 12:59:18 +0800

    init

--- translator.py
+++ translator.py
@@ -10,4 +10,29 @@ def init(proxy=""):
 
 
 def handler(text):
-    return trans.translate(text, dest="zh-cn")
+    results = split_string_with_delimiter(text)
+    texts = []
+    for result in results:
+        texts.append(trans.translate(result, dest="zh-cn"))
+
+    return "|||".join(texts)
+
+
+def split_string_with_delimiter(input_str, delimiter="|||", max_length=5000):
+    """
+    将输入字符串按照指定的分隔符分割，每段不超过指定长度
+    """
+    result = []
+    current_segment = ""
+
+    for part in input_str.split(delimiter):
+        if len(current_segment) + len(part) + len(delimiter) <= max_length:
+            current_segment += part + delimiter
+        else:
+            result.append(current_segment[:-len(delimiter)])  # 去掉末尾的分隔符
+            current_segment = part + delimiter
+
+        if current_segment and current_segment != delimiter:
+            result.append(current_segment[:-len(delimiter)])  # 处理最后一段
+
+    return result
